099f0a9c734dcbf7f2de2ad79523780107e9b188
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-04-18 14:24:48 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+Hooks.once("init", () => {
+  CONFIG.DND5E.weaponTypes.excelsiorShield = "Excelsior Shield";
+
+  CONFIG.DND5E.weaponTypeMap.excelsiorShield = "melee";
+
+  CONFIG.DND5E.weaponProficiencies.excelsiorShield = "Excelsior Shield";
+
+  CONFIG.DND5E.weaponProficienciesMap.excelsiorShield = "excelsiorShield";
+
+  CONFIG.DND5E.weaponMasteries.Bash = {
+      label: "Bash",
+      reference: "Compendium.excelsior-shields.excelsior-shields-journals.JournalEntry.VFEgqa4e7Y17B32Q.JournalEntryPage.I9WIMaDGo24PkO2p"
+  };
+  
+  CONFIG.DND5E.weaponMasteries.Brace = {
+      label: "Brace",
+      reference: "Compendium.excelsior-shields.excelsior-shields-journals.JournalEntry.VFEgqa4e7Y17B32Q.JournalEntryPage.cNqmwZLR00XrdtI0"
+  };
+  
+  CONFIG.DND5E.weaponMasteries.Counter = {
+      label: "Counter",
+      reference: "Compendium.excelsior-shields.excelsior-shields-journals.JournalEntry.VFEgqa4e7Y17B32Q.JournalEntryPage.Fjz56nlvNIlVFOM1"
+  };
+  
+  CONFIG.DND5E.weaponMasteries.Deflect = {
+      label: "Deflect",
+      reference: "Compendium.excelsior-shields.excelsior-shields-journals.JournalEntry.VFEgqa4e7Y17B32Q.JournalEntryPage.D3Yegb5GlXRCEDWE"
+  };
+  
+  CONFIG.DND5E.weaponMasteries.Interpose = {
+      label: "Interpose",
+      reference: "Compendium.excelsior-shields.excelsior-shields-journals.JournalEntry.VFEgqa4e7Y17B32Q.JournalEntryPage.s4Kzyz7RhEuNAFPk"
+  };
+
+  CONFIG.DND5E.weaponIds.buckler = "Compendium.excelsior-shields.excelsior-shield-items.Item.OI5JgbcRGweMkaK3";
+
+  CONFIG.DND5E.weaponIds.smallShield = "Compendium.excelsior-shields.excelsior-shield-items.Item.QEAATVCJgAL37IAO";
+
+  CONFIG.DND5E.weaponIds.roundShield = "Compendium.excelsior-shields.excelsior-shield-items.Item.qKoKzRrUkshDRuKF";
+
+  CONFIG.DND5E.weaponIds.parma = "Compendium.excelsior-shields.excelsior-shield-items.Item.bAtyn75nAiJ294TP";
+
+  CONFIG.DND5E.weaponIds.testudo = "Compendium.excelsior-shields.excelsior-shield-items.Item.dfbhw7D6AXKz3Db1";
+
+  CONFIG.DND5E.weaponIds.kite = "Compendium.excelsior-shields.excelsior-shield-items.Item.m3Tx5cmSIwSkAhe7";
+
+  CONFIG.DND5E.weaponIds.scutum = "Compendium.excelsior-shields.excelsior-shield-items.Item.47hWiPNXBi2f4zGL";
+
+  CONFIG.DND5E.weaponIds.largeShield = "Compendium.excelsior-shields.excelsior-shield-items.Item.D13sv27ctdECL6D9";
+
+  CONFIG.DND5E.weaponIds.pavise = "Compendium.excelsior-shields.excelsior-shield-items.Item.BPTPsV4ZK7agp5PB";
+
+  CONFIG.DND5E.weaponIds.spikedShield = "Compendium.excelsior-shields.excelsior-shield-items.Item.tEpfihwMy2BWAcQX";
+
+  CONFIG.DND5E.weaponIds.towerShield = "Compendium.excelsior-shields.excelsior-shield-items.Item.q9N6ld3fwAmqw0aS";
+
+  CONFIG.DND5E.weaponIds.greatShield = "Compendium.excelsior-shields.excelsior-shield-items.Item.2o0ryYfe9anBxwSU";
+});
